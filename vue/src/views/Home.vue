c3f43da46e51006d37488bd1675cfd0d93bec7b3
Nick <nnechaev93@gmail.com>
2022-12-06 13:40:28 -0500

    login page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+    <!-- <h1>Home</h1>
+    <p>You must be authenticated to see this</p> -->
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,38 +1,55 @@
 <template>
   <div id="login" class="text-center">
     <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
+      <!-- <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1> -->
+      <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+        Invalid username and password!
+      </div>
       <div
         class="alert alert-success"
         role="alert"
         v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
+      >
+        Thank you for registering, please sign in.
+      </div>
+
+      <label for="username" class="sr-only"></label>
+      <div class="login__field">
+        <i class="login__icon fas fa-user"></i>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Username"
+          v-model="user.username"
+          required
+          autofocus
+        />
+      </div>
+
+      <label for="password" class="sr-only"></label>
+      <div class="login__field">
+        <i class="login__icon fas fa-lock"></i>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          placeholder="Password"
+          v-model="user.password"
+          required
+        />
+      </div>
+      <button type="submit" class="sign-in-and-registration-buttons">
+        <i class="button__icon fas fa-chevron-right"></i>
+        Sign in
+      </button>
+
+      <router-link
+        tag="button"
+        class="sign-in-and-registration-buttons"
+        :to="{ name: 'register' }"
+        >Create an account</router-link
+      >
     </form>
   </div>
 </template>
@@ -47,30 +64,131 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push("/");
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
 
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style scope>
+@import url("https://fonts.googleapis.com/css?family=Raleway:400,700");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Raleway, sans-serif;
+}
+body #login {
+  background: linear-gradient(90deg, #c7c5f4, #776bcc);
+}
+
+.text-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+
+.form-signin {
+  background: linear-gradient(90deg, #5d54a4, #7c78b8);
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  padding: 30px;
+  padding-top: 120px;
+  box-shadow: 0px 0px 24px #5c5696;
+  /* z-index: 1; I have added z-index and height from original .screen__content, other staff is from .screen from original  */
+  height: 100%;
+  /**/
+  /* height: 600px; */
+  box-shadow: 0px 0px 24px #5c5696;
+}
+
+.login__field {
+  padding: 20px 0px;
+  position: relative;
+}
+
+.form-control {
+  border: none;
+  border-bottom: 2px solid #d1d1d4;
+  background: none;
+  padding: 10px;
+  padding-left: 24px;
+  font-weight: 700;
+  width: 75%;
+  transition: 0.2s;
+}
+
+.form-control:active,
+.form-control:focus,
+.form-control:hover {
+  outline: none;
+  border-bottom-color: #6a679e;
+}
+
+.sign-in-and-registration-buttons {
+  background: #fff;
+  font-size: 14px;
+  margin-top: 30px;
+  padding: 16px 20px;
+  border-radius: 26px;
+  border: 1px solid #d4d3e8;
+  text-transform: uppercase;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  width: 76%;
+  color: #4c489d;
+  box-shadow: 0px 2px 2px #5c5696;
+  cursor: pointer;
+  transition: 0.2s;
+}
+
+.sign-in-and-registration-buttons:active,
+.sign-in-and-registration-buttons:focus,
+.sign-in-and-registration-buttons:hover {
+  border-color: #6a679e;
+  outline: none;
+}
+.login {
+  width: 320px;
+  padding: 30px;
+  padding-top: 156px;
+}
+.login__field {
+  padding: 20px 0px;
+  position: relative;
+}
+
+.login__icon {
+  position: absolute;
+  top: 30px;
+  color: #7875b5;
+}
+</style>
