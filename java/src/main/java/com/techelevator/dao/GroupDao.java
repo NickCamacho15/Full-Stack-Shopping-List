47fc9bf527e0c5a97e46436deb77bc9aca31a135
Julian Jaszczak <julianjaszczak@gmail.com>
2022-12-10 20:55:25 +0000

    Revert "Added addgrouptouser functionality"
    This reverts commit 7f293128dfc35ee05bc996f1283f351e3d5fb14a

--- java/src/main/java/com/techelevator/dao/GroupDao.java
+++ java/src/main/java/com/techelevator/dao/GroupDao.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface GroupDao {
     List<Group> getGroups(String username);
     boolean createGroup(String username, String groupName);
-    boolean addUserToGroup(int userId, int groupId);
 }
